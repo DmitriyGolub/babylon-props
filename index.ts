0c4984323ce88f0964f41caeede8428968fc6779
DmitriyGolub <heartnesnev@gmail.com>
2023-08-02 00:41:56 +0300

    feat: page

--- index.ts
+++
@@ -1,7 +0,0 @@
-import {APPMANAGER} from "./src/managers/app-manager";
-import {App} from "./src/app";
-
-window.onload = () => {
-	APPMANAGER.setupGameManager()
-	new App().prepareScene()
-}
--- src/utils/get-canvas.ts
+++
@@ -1,9 +0,0 @@
-export const getCanvas = (): HTMLCanvasElement | null => {
-  const elementId = "canvas";
-  const canvas = document.getElementById(elementId);
-
-  if (!(canvas instanceof HTMLCanvasElement)) {
-    throw new Error(`Canvas with id ${elementId} doesn't exist`);
-  }
-  return canvas;
-};
